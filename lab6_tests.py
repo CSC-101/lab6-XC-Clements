7c526e6987d431006bdc1bf6d1771a1aa1456b54
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-11-06 10:21:10 -0800

    Oh shoot forgot to commit!!! Realized this in class today :(

--- lab6_tests.py
+++ lab6_tests.py
@@ -1,6 +1,8 @@
 import data
 import lab6
 import unittest
+from data import Book
+from lab6 import selection_sort_books, swap_case, str_translate, histogram
 
 
 # Write your test cases for each part below.
@@ -64,17 +66,28 @@ class TestCases(unittest.TestCase):
 
 
     # Part 1
+    def test_selection_sort_books(self):
+        self.assertEqual(selection_sort_books([Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens'),
+                              Book(["J.K. Rowling"], "Harry Potter and the Chamber of Secrets"),
+                              Book(["tester"], "Wahoo!!"), Book([], "Bello")]), [Book([], 'Bello'), Book(['Neil Gaiman', 'Terry Pratchett'], 'Good Omens'), Book(['J.K. Rowling'], 'Harry Potter and the Chamber of Secrets'), Book(['tester'], 'Wahoo!!')])
+    def test_selection_sort_books_2(self):
+        self.assertEqual(selection_sort_books([]), None)
 
-
-    # Part 2
-
-
+        # Part 2
+    def test_swap_case(self):
+        self.assertEqual(swap_case("hi"), "HI")
+    def test_swap_case_2(self):
+        self.assertEqual(swap_case("heLLo! GoOd to SEE you"), "HEllO! gOoD TO see YOU")
     # Part 3
-
-
+    def test_str_translate(self):
+        self.assertEqual(str_translate("hello!", "l", "p"), "heppo!")
+    def test_str_translate_2(self):
+        self.assertEqual(str_translate("welcome!", "q", "m"), "welcome!")
     # Part 4
-
-
+    def test_histogram(self):
+        self.assertEqual(histogram("hello my name is name"), {'hello': 1, 'my': 1, 'name': 2, 'is': 1})
+    def test_histogram_2(self):
+        self.assertEqual(histogram("hello i! am a tlkaking cat!!"), {'hello': 1, 'i!': 1, 'am': 1, 'a': 1, 'tlkaking': 1, 'cat!!': 1})
 
 
 
